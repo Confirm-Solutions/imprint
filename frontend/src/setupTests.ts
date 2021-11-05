d7788bbb47bc3dc049d80606b5b1540a428c0197
James Yang <jamesyang916@gmail.com>
2021-11-04 21:24:18 -0700

    Finish optimizing and refactoring for bckt model

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import 'jest-canvas-mock';
+
+window.URL.createObjectURL = jest.fn();
+
+afterEach(() => {
+    window.URL.createObjectURL.mockReset();
+});
