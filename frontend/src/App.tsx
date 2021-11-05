efefcf4f6299618612b468183543dc3f2880e94b
James Yang <jamesyang916@gmail.com>
2021-11-04 21:24:18 -0700

    Finish optimizing and refactoring for bckt model

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,28 @@ import Canvas from './canvas';
 import { PlotType } from './canvas';
 import React from 'react';
 import './App.css';
-import { FormControl, FormLabel, FormControlLabel, Radio, RadioGroup } from '@mui/material';
+import { FormControl, FormLabel, FormControlLabel, Radio, RadioGroup, Checkbox } from '@mui/material';
 
+const numLayers = 6;
+const layerNames = ["Monte Carlo Type I error estimates", "0th order upper bound", "Max gradient estimates", "1st order upper bound", "2nd order upper bound", "Total upper bound"]
 
 function App() {
   const [plotType, setPlotType] = React.useState<PlotType>("surface");
+  const getDefaultState = () => {
+    let s = new Array(numLayers).fill(false)
+    s[numLayers - 1] = true;
+    return s
+  }
+  const [checkboxStates, setCheckboxStates] = React.useState<boolean[]>(getDefaultState())
+
+  const checkboxes = checkboxStates.map((value, i) => {
+    return <FormControlLabel key={i} label={layerNames[i]} control={<Checkbox checked={value} onChange={(_, v) => {
+      let newState = Array.from(checkboxStates);
+      newState[i] = v;
+      setCheckboxStates(newState)
+    }} />} />
+  })
+
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>, value: string) {
     switch (value) {
@@ -19,7 +36,7 @@ function App() {
 
   return (
     <div className="row">
-      <div className="column left">
+      <div className="left">
         <FormControl>
           <FormLabel id="demo-radio-buttons-group-label">Plot Type</FormLabel>
           <RadioGroup
@@ -32,20 +49,23 @@ function App() {
             <FormControlLabel value="scatter3d" control={<Radio />} label="Scatter" />
           </RadioGroup>
         </FormControl>
+        <FormControl>
+          <FormLabel id="demo-radio-buttons-group-label">Layers Shown</FormLabel>
+          {checkboxes}
+        </FormControl>
       </div>
-      <div className="column middle">
+      <div className="right">
         <article>
           <header>
             <h1>Simulated Family-Wise Error Rates</h1>
           </header>
           <main>
-            <Canvas plotType={plotType}></Canvas>
+            <Canvas plotType={plotType} checkboxStates={checkboxStates}></Canvas>
           </main>
           <footer>
           </footer>
         </article>
       </div>
-      <div className='column right'></div>
     </div>
   );
 }
