7ca10e4eedf7aaf5a44878a86113f3233c3f821a
Alex Constantino <me@alexconstantino.com>
2022-02-17 10:33:56 -0500

    Allow choosing between scatter and surface plots
    And update test CSV with corrected data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,45 @@
 import Canvas from './canvas';
+import { PlotType } from './canvas';
+import React from 'react';
 import './App.css';
+import { FormControl, FormLabel, FormControlLabel, Radio, RadioGroup } from '@mui/material';
 
 
 function App() {
+  const [plotType, setPlotType] = React.useState<PlotType>("surface");
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>, value: string) {
+    switch (value) {
+      case "surface":
+        return setPlotType(value);
+      case "scatter3d":
+        return setPlotType(value);
+    }
+  }
+
   return (
     <div className="row">
-      <div className="column left"></div>
+      <div className="column left">
+        <FormControl>
+          <FormLabel id="demo-radio-buttons-group-label">Plot Type</FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="surface"
+            name="radio-buttons-group"
+            onChange={handleChange}
+          >
+            <FormControlLabel value="surface" control={<Radio />} label="Surface" />
+            <FormControlLabel value="scatter3d" control={<Radio />} label="Scatter" />
+          </RadioGroup>
+        </FormControl>
+      </div>
       <div className="column middle">
         <article>
           <header>
             <h1>Simulated Family-Wise Error Rates</h1>
           </header>
           <main>
-            <Canvas></Canvas>
+            <Canvas plotType={plotType}></Canvas>
           </main>
           <footer>
           </footer>
