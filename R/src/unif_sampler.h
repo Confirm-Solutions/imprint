0e3dc850998ef75f3ee9118d8163f63ad10e27b0
James Yang <jamesyang916@gmail.com>
2022-03-23 17:30:47 -0400

    Clean R Legacy and Revamp build system (#38)
    * Add extra dependency and simplify dependency graph
    
    * Move legacy R stuff into R
    
    * Revamp build system and clean R legacy
    
    * Add explanation for why we have dummy BUILD
    
    * Add vscode stuff back

--- R/src/unif_sampler.h
+++
@@ -1,9 +0,0 @@
-#include <random>
-#include <Eigen/Core>
-
-template <class GenType, class RNGType>
-void unif_sampler(GenType& gen, RNGType& rng, size_t m, size_t n) {
-    std::uniform_real_distribution<double> unif(0., 1.);
-    rng = Eigen::MatrixXd::NullaryExpr(m, n,
-                                       [&](auto, auto) { return unif(gen); });
-};
