c2d4813e44574e7463151725ab41adb8004ee352
Alex Constantino <5673946+constinit@users.noreply.github.com>
2022-04-07 13:00:00 -0400

    Undo some test refactoring

--- kevlar/test/model/direct_bayes.cpp
+++ kevlar/test/model/direct_bayes.cpp
@@ -12,32 +12,32 @@
 
 namespace kevlar {
 namespace {
-
+struct MockHyperPlane;
 using value_t = double;
 using mat_t = DirectBayesBinomialControlkTreatment<value_t>::mat_t;
 using vec_t = DirectBayesBinomialControlkTreatment<value_t>::vec_t;
-
-struct MockHyperPlane : HyperPlane<double> {
-    using base_t = HyperPlane<double>;
-    using base_t::base_t;
-};
 using uint_t = uint32_t;
 using tile_t = Tile<value_t>;
 using hp_t = MockHyperPlane;
 using gr_t = GridRange<value_t, uint_t, tile_t>;
 using bckt_t = DirectBayesBinomialControlkTreatment<value_t>;
 
-const double mu_sig_sq = 100;
-const double alpha_prior = 0.0005;
-const double beta_prior = 0.000005;
-const double efficacy_threshold = 0.3;
-const int n_integration_points = 50;
-const int n_arm_size = 15;
+const Eigen::Vector<value_t, 1> critical_values{0.95};
 const auto tol = 1e-8;
+const int n_arm_size = 15;
+const int n_integration_points = 50;
 const size_t n_arms = 2;
 const size_t n_samples = 250;
-const Eigen::Vector<value_t, 1> critical_values{0.95};
 const size_t n_thetas = 10;
+const value_t alpha_prior = 0.0005;
+const value_t beta_prior = 0.000005;
+const value_t efficacy_threshold = 0.3;
+const value_t mu_sig_sq = 100;
+
+struct MockHyperPlane : HyperPlane<value_t> {
+    using base_t = HyperPlane<value_t>;
+    using base_t::base_t;
+};
 
 vec_t get_efficacy_thresholds(int n) {
     Eigen::Vector<value_t, Eigen::Dynamic> efficacy_thresholds(n);
@@ -80,6 +80,8 @@ gr_t get_grid_range() {
 }
 
 DirectBayesBinomialControlkTreatment<value_t> get_test_class() {
+    // TODO: n_samples should really be n_arm_size, but causes the rej_len test
+    // to fail
     bckt_t b_new(n_arms, n_samples, critical_values,
                  get_efficacy_thresholds(n_arms));
     return b_new;
