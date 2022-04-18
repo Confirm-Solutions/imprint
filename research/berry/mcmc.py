dd561498cc0ab4f53fe06dbdb200d1d6fd6a0f51
constinit <5673946+constinit@users.noreply.github.com>
2022-04-17 20:51:22 -0400

    Fix MCMC parallel evaluation (#76)
    It should use jax.pmap; putting everything in one chain causes too many
    divergences.
    
    It now collects 500,000 Berry samples for all 6 analyses in 11 seconds.

--- research/berry/mcmc.py
+++ research/berry/mcmc.py
@@ -1,3 +1,7 @@
+# flake8: noqa E402
+import os
+
+os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=6"
 import jax
 import numpy as np
 import numpyro
@@ -8,42 +12,56 @@ from numpyro.infer import MCMC, NUTS
 def mcmc_berry(model, data, suc_thresh):
     logit_p1 = model.logit_p1
 
-    def mcmc_berry_model(ys, ns):
-        with numpyro.plate("i", 6, dim=-2):
-            mu = numpyro.sample("mu", dist.Normal(-1.34, 10))
-            sigma2 = numpyro.sample("sigma2", dist.InverseGamma(0.0005, 0.000005))
-            with numpyro.plate("j", 4, dim=-1):
-                theta = numpyro.sample(
-                    "theta",
-                    dist.Normal(mu, jax.numpy.sqrt(sigma2)),
-                )
-                numpyro.sample(
-                    "y",
-                    dist.BinomialLogits(theta + logit_p1, total_count=ns),
-                    obs=ys,
-                )
+    def mcmc_berry_model(y, n):
+        mu = numpyro.sample("mu", dist.Normal(-1.34, 10))
+        sigma2 = numpyro.sample("sigma2", dist.InverseGamma(0.0005, 0.000005))
+        with numpyro.plate("j", 4):
+            theta = numpyro.sample(
+                "theta",
+                dist.Normal(mu, jax.numpy.sqrt(sigma2)),
+            )
+            numpyro.sample(
+                "y",
+                dist.BinomialLogits(theta + logit_p1, total_count=n),
+                obs=y,
+            )
 
-    mcmc_stats = {}
-    mcmc_data = []
-    nuts_kernel = NUTS(mcmc_berry_model)
-    mcmc = MCMC(
-        nuts_kernel,
-        num_warmup=5000,
-        num_samples=200_000,
-        thinning=5,
-        progress_bar=False,
+    def do_mcmc(rng_key, y, n):
+        nuts_kernel = NUTS(mcmc_berry_model)
+        mcmc = MCMC(
+            nuts_kernel,
+            progress_bar=False,
+            num_warmup=10_000,
+            num_samples=500_000,
+            thinning=5,
+        )
+        mcmc.run(rng_key, y, n)
+        return mcmc.get_samples(group_by_chain=True)
+
+    # Number of devices to pmap over
+    n_parallel = data.shape[0]
+    rng_keys = jax.random.split(jax.random.PRNGKey(0), n_parallel)
+    traces = jax.pmap(do_mcmc)(rng_keys, data[..., 0], data[..., 1])
+    # concatenate traces along pmap'ed axis
+
+    mcmc_stats = dict(
+        cilow=np.empty((6, 4)),
+        cihi=np.empty((6, 4)),
+        theta_map=np.empty((6, 4)),
+        exceedance=np.empty((6, 4)),
     )
-    rng_key = jax.random.PRNGKey(0)
-    mcmc.run(rng_key, data[..., 0], data[..., 1])
-    x = mcmc.get_samples(group_by_chain=True)
-    summary = numpyro.diagnostics.summary(x, prob=0.95)
-    mcmc_stats["cilow"] = summary["theta"]["2.5%"]
-    mcmc_stats["cihi"] = summary["theta"]["97.5%"]
-    mcmc_stats["theta_map"] = summary["theta"]["mean"]
-    n_samples = x["theta"].shape[0] * x["theta"].shape[1]
-    mcmc_stats["exceedance"] = np.sum(x["theta"] > suc_thresh, axis=(0, 1)) / n_samples
-    mcmc_data.append(mcmc)
-    return mcmc_data, mcmc_stats
+    assert data.shape[0] == 6
+    for i in range(6):
+        x = {k: v[i] for k, v in traces.items()}
+        summary = numpyro.diagnostics.summary(x, prob=0.95)
+        mcmc_stats["cilow"][i] = summary["theta"]["2.5%"]
+        mcmc_stats["cihi"][i] = summary["theta"]["97.5%"]
+        mcmc_stats["theta_map"][i] = summary["theta"]["mean"]
+        n_samples = x["theta"].shape[0] * x["theta"].shape[1]
+        mcmc_stats["exceedance"][i] = (
+            np.sum(x["theta"] > suc_thresh[i], axis=(0, 1)) / n_samples
+        )
+        return mcmc_stats
 
 
 if __name__ == "__main__":
